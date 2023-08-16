21a27e876ed1b45bd9ba2fe2914b5f81353deb8d
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-08-16 19:44:27 +0200

    HW 8 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
 import VimeoPlayer from "@vimeo/player"
-import throttle from "lodash.throttle"
 
 
 const iframe = document.querySelector("iframe")
 const player = new VimeoPlayer(iframe)
-const updateLocalStorage = throttle(function(currentTime){
-    localStorage.setItem("videoplayer-current-time", currentTime.toString())
-}, 1000)
 
 player.on("timeupdate", function(data) {
     const currentTime = data.seconds
     updateLocalStorage(currentTime)
 })
+const updateLocalStorage = _.throttle(function(currentTime){
+    localStorage.setItem("videoplayer-current-time", currentTime.toString())
+}, 1000)
+
 
 const savedTime = localStorage.getItem("videoplayer-current-time")
 let parsedTime = 0;
@@ -23,7 +23,7 @@ player.setCurrentTime(parsedTime).then(function(seconds){}).catch(function(error
 switch(error.name) {
     case "RangeError": alert ("Czas był mniejszy niż 0 lub większy niż czas trwania wideo.")
     break;
-    default: alert("Wystąpił błąd:", + error.message)
+    default: alert("Wystąpił błąd:" + error.message)
     break
 }
 }) 
