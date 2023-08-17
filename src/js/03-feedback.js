b44b7d259cfd9056bcd45c93019cadd794b76c4b
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-08-17 19:16:54 +0200

    HW 8 form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,39 @@
 import throttle from "lodash.throttle"
-const form = document.querySelector('.feedback-form')
-const email = form.elements.email
-const message = form.elements.message
+const form = document.querySelector(".feedback-form")
+const emailInput = document.querySelector('[name="email"]')
+const messageInput = document.querySelector('[name="message"]')
+
+form.addEventListener("input", throttle((event) => {
+    saveMessage(event)
+},500))
 
 
- const formData = {
-        email: email.value,
-        message: message.value
-    }
+function saveMessage(event) {
+    event.preventDefault()
+const { elements: {email, message}} = form
+const formData = {
+    email: email.value,
+    message: message.value
+}
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}
 
+const savedData = localStorage.getItem("feedback-form-state")
+if (savedData !== null) {
+    const parsedData = JSON.parse(savedData)
+    emailInput.value = parsedData.email
+    messageInput.value = parsedData.message
+}
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+   const dataToLog = {
+    email: emailInput.value,
+    message: messageInput.value
+   }
+   console.log(dataToLog)
+    localStorage.removeItem("feedback-form-state")
+    event.currentTarget.reset()
+})
 
 
